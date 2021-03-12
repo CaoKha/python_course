879d99a82b6c66a470128bc9242d28d40c0a7171
Kha Nguyen Cao <nguyencaokha131995@gmail.com>
2021-03-12 15:48:19 +0100

    first commit

--- scripts/numpy_example.py
+++ scripts/numpy_example.py
@@ -1,3 +1,13 @@
-import numpy as np 
-a = np.array([1,2,3]) 
-print(a)
+import numpy as np
+# a = np.array([1, 2, 3])
+# print(a)
+
+# # dtype parameter
+# a = np.array([1, 2, 3], dtype=complex)
+# print(a)
+
+a = np.array([1, 2, 3, 4, 5])
+np.save('/home/kha/Documents/python_course/files/outfile', a)
+
+b = np.load('files/outfile.npy') 
+print(b)
