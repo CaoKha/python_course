af65655027a4c69540c74bb703a3f31b691d345c
Kha Nguyen Cao <nguyencaokha131995@gmail.com>
2021-03-12 17:27:27 +0100

    first commit

--- scripts/numpy_example.py
+++ scripts/numpy_example.py
@@ -1,13 +1,20 @@
 import numpy as np
+import sympy as sp
+
 # a = np.array([1, 2, 3])
 # print(a)
 
-# # dtype parameter
+# # complex number with numpy
 # a = np.array([1, 2, 3], dtype=complex)
 # print(a)
 
-a = np.array([1, 2, 3, 4, 5])
-np.save('/home/kha/Documents/python_course/files/outfile', a)
+# # file handle
+# a = np.array([1, 2, 3, 4, 5])
+# np.save('/home/kha/Documents/python_course/files/outfile', a)
 
-b = np.load('files/outfile.npy') 
-print(b)
+# b = np.load('files/outfile.npy')
+# print(b)
+
+x = sp.symbols("x")
+y = sp.symbols("y")
+z = sp.symbols("z")
