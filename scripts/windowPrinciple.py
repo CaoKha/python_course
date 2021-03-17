77e9a7f7ee2db10479b7df907892b72bf9d25233
CaoKha <nguyencaokha131995@gmail.com>
2021-03-17 09:46:19 +0100

    first commit

--- scripts/windowPrinciple.py
+++ scripts/windowPrinciple.py
@@ -20,17 +20,22 @@ class windowMain(QtWidgets.QWidget):
         self.objectMyLineEdit = QtWidgets.QLineEdit(
             "Type the message you want copying!"
         )
+        self.objectMyTextEdit = QtWidgets.QTextEdit("Type the text you want to saving!")
         self.objectMyRadioDeleteButton = QtWidgets.QRadioButton("Delete")
         self.objectMyRadioCopyButton = QtWidgets.QRadioButton("Copy")
+        self.objectMySaveButton = QtWidgets.QPushButton("Save")
 
         # The placement of vidgets
         v_box = QtWidgets.QVBoxLayout()
-        v_box.addWidget(self.objectMyRadioDeleteButton)
-        v_box.addWidget(self.objectMyRadioCopyButton)
         v_box.addWidget(self.objectMyLabel)
         v_box.addWidget(self.objectMyLineEdit)
+        v_box.addWidget(self.objectMyTextEdit)
+        v_box.addWidget(self.objectMyRadioDeleteButton)
+        v_box.addWidget(self.objectMyRadioCopyButton)
         v_box.addWidget(self.objectMyEraseButton)
         v_box.addWidget(self.objectMyCopyButton)
+        v_box.addWidget(self.objectMySaveButton)
+
 
         # Mail application
         self.setLayout(v_box)
@@ -42,6 +47,8 @@ class windowMain(QtWidgets.QWidget):
         self.objectMyEraseButton.clicked.connect(
             lambda: self.EraseClickedHandler(self.objectMyRadioDeleteButton.isChecked())
         )
+
+        self.objectMySaveButton.clicked.connect(self.SaveFunction)
 
     def CopyClickedHandler(self, verifier):
         if verifier:
@@ -69,3 +76,10 @@ class windowMain(QtWidgets.QWidget):
         else:
             self.objectMyCopyButton.setDisabled(False)
             self.objectMyEraseButton.setDisabled(True)
+
+    def SaveFunction(self):
+        with open("files/saved_text_from_PyQt5.txt","w") as f:
+            my_text = self.objectMyTextEdit.toPlainText()
+            f.write(my_text)
+            print("your text is saved in folder named \"files\"")
+            QMessageBox.about(self,"Information Message","Your text is saved with success!")
